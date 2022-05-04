c3027880ddab6d8ccb6932f68e76ae72feffeada
fzuljevic <fzuljevic96@gmail.com>
2022-05-04 22:21:02 +0200

    add header and footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,16 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.scss'
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
-      Hellow!
-    </div>
+    <>
+      <header className={styles["page__header"]}>Dummie header</header>
+      <main className={styles.main}>
+      </main>
+      <footer className={styles["page__footer"]}>All rights reserved. &copy;</footer>
+    </>
   )
 }
 
