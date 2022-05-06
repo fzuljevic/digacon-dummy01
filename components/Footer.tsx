5e349bf1076bc227a1da55d862063e3efc13d214
fzuljevic <fzuljevic96@gmail.com>
2022-05-06 11:11:42 +0200

    adds translations

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,20 @@
+import { useTranslations } from 'next-intl';
 import styles from './Footer.module.scss'
 
 function Footer ()  {
+  const t = useTranslations('home')
+
   return (
-    <footer className={styles.footer}>All rights reserved. &copy;</footer>
+    <footer className={styles.footer}>{t('footer')} &copy;</footer>
     )
 }
 
 export default Footer
+
+export async function getStaticProps({locale} : any) {
+  return {
+    props: {
+      messages: (await import(`../messages/${locale}.json`)).default
+    }
+  };
+}
